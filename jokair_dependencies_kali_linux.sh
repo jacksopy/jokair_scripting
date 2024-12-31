4148ee321fb6085868786ba793d77434ffb8cbd9
jacksopy <148472316+jacksopy@users.noreply.github.com>
2024-12-31 16:15:14 +0100

    Create jokair_dependencies_kali_linux.sh

---
+++ jokair_dependencies_kali_linux.sh
@@ -0,0 +1,7 @@
+sudo apt install nmap
+sudo apt install john
+sudo apt install gobuster
+sudo apt install hydra
+sudo apt install sqlmap
+sudo apt install john-data
+sudo apt install metasploit-framework
