c1eea11c6966235628ee9973970f3d3be006b0e0
jacksopy <148472316+jacksopy@users.noreply.github.com>
2024-12-31 16:14:36 +0100

    jokair_scripting.sh

---
+++ jokair_scripting.sh
@@ -0,0 +1,241 @@
+#!/bin/bash
+clear 
+
+
+echo
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣄⣀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⠋⠙⢿⣷⣄"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠉⠻⣷"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⡇⠀⠀⠀⠀⠀⠙⢿⣧⣀⣀⣤⣴⣶⣦"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠉⢿⣿⣿⣿⢿⣿" 
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣟⠀⣀⣀⣀⣀⣤⣴⣶⣾⣿⠿⠟⠁⣼⣿⣶⣶⣶⣶⣶⣤⣄⣀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣿⣿⡿⠿⠛⠋⠉⠉⠉⠉⠁⠀⠀⠀⣰⣿⡟⠉⠉⠉⠛⠻⠿⣿⣿⣿⣷⣶⣤⣀"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⣤⡾⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣴⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⢿⣿⣿⣿⣦⣄"
+echo "⠀⠀⠀⠀⠀⠀⠀⢿⣟⠀⠀⠀⠀⢀⣀⣤⣤⡶⠾⠿⠛⠛⢉⣡⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣿⣿⣦⣄"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠛⢷⣄⣠⣶⠿⠋⠉⠀⠀⠀⠀⠀⢠⣾⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣿⣿⣷⣄"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣅⠀⠀⠀⠐⣿⣦⠀⠀⣾⣿⣿⡟⠀⠀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣷⣄"
+echo "⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣷⣄⠀⠀⠙⣿⡄⢸⣿⣿⡿⠁⣠⣾⡿⠋⠀⠀⢀⣀⣤⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣧."
+echo "⠀⠀⠀⠀⠀⠀⣰⣿⡟⠁⠀⠉⠛⢿⣿⣧⡀⠀⠈⣇⣸⣿⡿⢁⣼⠟⠁⢀⣠⣶⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣷⡀"
+echo "⠀⠀⠀⠀⠀⣸⣿⡟⠀⠀⠀⠀⠀⠀⠙⢿⣿⡄⠀⠿⣿⡿⣳⠞⠁⣠⣾⣿⣿⠿⠛⠉⠉⠙⠻⢿⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣷"
+echo "⠀⠀⠀⠀⢰⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⡀⠀⣿⠇⠃⣠⣾⣿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣧"
+echo "⠀⠀⠀⠀⣼⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⠀⠋⢠⣼⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿"
+echo "⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠈⣿⡆⣰⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿"
+echo "⠀⠀⠀⢠⣿⣿⠀⠀⠀⠀⣀⡀⠀⠀⢻⣷⡄⠀⢻⣷⣿⡟⠁⠀⢀⣠⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿"
+echo "⠀⠀⠀⠈⣿⣿⠀⠀⣠⣾⠿⣿⣦⡄⠈⢻⣿⠀⢸⣿⠟⠀⠀⢠⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿"
+echo "⠀⠀⢀⣴⣿⣿⣦⡾⢿⣿⠿⠿⠿⢿⣷⣤⣿⠀⠘⠛⠀⠀⢠⣿⢋⣴⣾⠿⣿⣶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡟"
+echo "⠀⣴⣿⡿⠋⠁⣠⣶⡿⠁⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⠀⠀⠙⠿⠿⠿⠶⢿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⠀⣸⣿⠆⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿"
+echo "⣾⣿⣿⠀⠀⣼⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣯⠉⠙⠻⠿⠦⠀⠀⠀⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠇"
+echo "⢸⣿⣿⠏⠀⢸⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⣿⡇⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣏⣀" 
+echo "⢸⣿⣿⠀⣿⠀⠀⠀⠀⠀⢀⣀⣀⣀⣠⣴⡿⢿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣷⠀⠀⠀⠀⠀⣀⣠⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠛⠛⠛⠿⣿⣦" 
+echo "⠸⣿⣿⡆⢹⣦⠀⠀⠀⣾⣿⣩⣽⣿⣯⣶⣾⣶⣶⣍⢻⣷⣦⣀⡀⠀⠀⠀⠀⠀⢻⣿⣿⠀⠀⠀⠀⠀⣿⣿⣿⣿⣆⢸⣷⡀⠀⠀⠀⠀⠀⢀⣠⣤⣄⣿⣿"
+echo "⣿⡟⣿⣄⠻⣷⣦⣀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠉⠁⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⡆⢿⣇⠀⠀⠀⠀⣼⡟⠋⠉⢹⣿⡿"
+echo "⢹⣿⣾⡿⣦⡀⠉⠛⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⣿⣿⣿⣿⡇⣸⡇⠀⠀⠀⣼⡟⠀⠀⢀⣿⣿"
+echo "⠀⣿⣇⢿⣟⠙⣷⣄⠀⠀⠀⠀⢙⣿⣿⣿⣿⣿⠟⠻⠿⠿⠿⠛⠛⢷⣦⣤⣤⣤⣤⣴⣶⠿⠛⣀⣴⣾⣿⣿⣿⡿⢡⣿⠀⠀⠀⣠⣿⡇⠀⠀⣼⣿⠃"
+echo "⠀⠸⣿⡌⢿⣶⣿⠉⣿⣶⣶⣾⣿⣇⠀⢉⣽⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠁⠉⢁⣠⣶⡾⠛⣹⣷⢯⣿⡿⢃⣾⠃⠀⣠⣾⣿⣿⡇⠀⣸⣿⠃"
+echo "⠀⠀⢻⣧⠈⢻⣿⣤⣿⠀⠈⣏⠛⠿⣷⣿⣿⣿⣿⣦⣀⣀⣠⣤⣤⣀⣀⣶⡶⠿⠛⠿⠋⣹⣠⣿⡿⢋⣾⡿⠁⠞⠁⠀⠀⣿⡿⠛⠋⠀⣰⣿⠃"
+echo "⠀⠀⢸⣿⡇⠀⢻⣿⣿⣷⣦⣯⡀⠀⣼⠀⠀⠀⢹⡏⠉⠉⣿⠁⠀⢠⡇⠀⣸⣆⣠⣶⣶⣿⡿⠟⢀⣾⡟⠁⠀⠀⠀⢠⣾⡿⠀⠀⢀⣼⡿⠃"
+echo "⠀⠀⠈⣿⣧⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣤⣾⣷⣤⣴⣿⣶⣶⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⣠⣿⠟⠀⠀⠀⠀⠀⠈⠉⠀⣠⣶⡿⠛⠁"
+echo "⠀⠀⠀⢹⣿⡇⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⢀⣴⠟⠁⠀⠀⠀⢀⣶⠀⠀⠀⣾⡿⠋"
+echo "⠀⠀⠀⠘⣿⡇⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⢰⡟⠁⠀⠀⠀⠀⠀⠈⣿⣷⣤⣼⡿⠁"
+echo "⠀⠀⠀⠀⣿⡇⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⡿⠛⠉"
+echo "⠀⠀⠀⠀⢸⡇⠀⣿⠷⣤⣽⣷⣾⣿⡉⢉⣻⣟⠛⣿⠿⠿⡿⠿⢿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⠏"
+echo "⠀⠀⠀⠀⢸⣧⠀⢿⣷⣶⣭⣥⣼⣭⣙⣿⣛⠛⠿⠿⣶⠶⠷⢶⣾⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⡿⠏"
+echo "⠀⠀⠀⠀⠸⣿⡄⠀⠙⠻⠿⠛⠛⠛⠻⣿⣿⣿⣦⣤⣤⣤⣶⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⠟⠁"
+echo "⠀⠀⠀⠀⠀⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣿⠟⠁"
+echo "⠀⠀⠀⠀⠀⢸⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣾⡿⠛⠁"
+echo "⠀⠀⠀⠀⠀⠈⢻⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⡿⠟⠉"
+echo "⠀⠀⠀⠀⠀⠀⠀⠙⠿⣿⣶⣤⣶⣾⣷⣄⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⠟⠋"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁⠈⠙⢿⣿⣷⣶⣶⣶⣾⣿⡿⠟⠋⠁"
+echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠈⢉⠉⠉⠉⢉⠀⢀⣀⡀"
+echo                                          
+echo
+
+
+echo "-----------------------------------------------------------"
+echo "                        TOOLS"
+echo "+-------+---------+------+--------+--------+--------------+"
+echo "/ nmap  / gobuster/ hydra / sqlmap / john   /  metasploit /"    
+echo "+-------+---------+------+--------+--------+--------------+"
+echo "/   1   /    2    /  3   /    4   /    5   /       6      /"
+echo "+-------+---------+------+--------+--------+--------------+"
+
+
+
+echo "choose a tool" 
+read tool_choice
+case $tool_choice in
+    1)
+            echo "nmap"
+        read ip_adress
+        nmap -A -Pn $ip_adress
+        ;;
+    2) 
+        echo "gobuster"
+        echo "2.1 directories enumeration"
+        echo "2.2 dns enumeration"
+        echo "2.3 vhost enumeration"
+
+        echo "choose an option"
+        read gobuster_option
+
+        case $gobuster_option in
+            2.1) 
+                echo "directory enumeration"
+                echo "choose dns or ip adress to enumerate"
+                read dns_or_ip_address
+                echo "put the path of you're wordlist"
+                read wordlist
+                gobuster dir -u http://$dns_or_ip_adress/ -w  $wordlist 
+                ;;
+            
+            2.2)
+                echo "domain name system enumeration"
+                echo "put the domain name"
+                read domain_name
+                echo "put the path of you're wordlist"
+                read wordlist
+                gobuster dns -d $domain_name -t 50 -w $wordlist
+                ;;
+            
+            2.3)
+                echo "vhost enumeration"
+                echo "put the domain name"
+                read domain_name
+                echo "put the path of you're wordlist"
+                read wordlist
+                gobuster vhost -u http://$domain_name/ -w $wordlist
+                ;;
+        esac
+        ;;
+    3)
+        echo "hydra"
+        echo "3.1 login ssh brute force authentication"
+        echo "3.2 login ftp brute force authentication"
+        echo "3.3 login mssql brute force authentication"
+        echo "3.4 login mysql brute force authentication"
+        echo "3.5 login smb brute force authentication"
+        echo "3.6 login rdp brute force authentication"
+        echo "3.7 login http basic authentication"
+        echo "3.8 login http login page authentication"
+
+
+        echo "choose an option"
+        read hydra_option
+        
+        case $hydra_option in
+            3.1)
+                echo "ssh brute force"
+                echo "put the ip address"
+                read ip_address
+                echo "put the port"
+                read port
+                echo "put the username"
+                read username
+                echo "put the path of you're wordlist"
+                read wordlist
+                hydra -l $username -P $wordlist $ip_address:$port ssh -v
+                ;;
+
+            3.2)
+                echo "ftp brute force"
+                echo "put the ip address"
+                read ip_address
+                echo "put the port"
+                read port
+                echo "put the username"
+                read username
+                echo "put the path of you're wordlist"
+                read wordlist
+                hydra -l $username -P $wordlist ftp://$ip_address -v
+                ;;
+
+            3.3)
+                echo "mssql brute force"
+                echo "put the ip address"
+                read ip_address
+                echo "put the username"
+                read username
+                echo "put the path of you're wordlist"
+                read wordlist
+                hydra -l $username -P $wordlist mssql -v
+                ;;
+            3.4)
+                echo "mysql brute force"
+                echo "put the ip address"
+                read ip_address
+                echo "put the username"
+                read username
+                echo "put the path of you're wordlist"
+                read wordlist
+                hydra -l $username -P $wordlist mysql://$ip_address -v
+                ;;
+
+            3.5)
+                echo "smb brute force"
+                echo "put the ip address"
+                read ip_address
+                echo "put the username"
+                read username
+                echo "put the path of you're wordlist"
+                read wordlist
+                hydra -l $username -P $wordlist smb://$ip_address -v
+                ;;
+
+            3.6)
+                echo "rdp brute force"
+                echo "put the ip address"
+                read ip_address
+                echo "put the username"
+                read username
+                echo "put the path of you're wordlist"
+                read wordlist
+                hydra -l $username -P $wordlist rdp://$ip_address -v
+                ;;
+            3.7)
+                echo "http basic brute force"
+                echo "put the ip address"
+                read ip_address
+                echo "put the port"
+                read port
+                echo "put the path of you're wordlist"
+                read wordlist
+                hydra -l $username -P $wordlist http://$ip_address -v http-get / 
+                ;;
+            3.8)
+               echo "3.8 login http login page authentication"
+               echo "put the ip address or dns"
+               read ip_or_dns
+               echo "put the username"
+               read username
+               echo "put the path of you're wordlist"
+               read wordlist
+               echo "put the form name"
+               read form_name
+               hydra -l $username -P $wordlist -f $ip_or_dns -s $port http-post-form "/$form_name:username=^USER^&password=^PASS^:F=<form name='login'"
+                ;;
+        esac
+        ;;
+    4) 
+        echo "sqlmap"
+        echo "4.1 scrappers located"
+        echo "4.2 crawler"
+        read sqlmap
+        case "$sqlmap" in
+        4.1) 
+            echo "sqlmap scrappers located"
+            echo "put the url"
+            read url
+            sqlmap -u http://$url --batch --dump
+            ;;
+        4.2)  
+            echo "sqlmap crawlers"
+            echo "put the url"
+            read url
+            sqlmap -u http://$url --batch --crawl=2 --dump
+        esac
+        ;;
+    5)
+
+
+
+            
