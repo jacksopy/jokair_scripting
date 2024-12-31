c39b2e02f1c98984246714337ee5cd284fa09095
jacksopy <148472316+jacksopy@users.noreply.github.com>
2024-12-31 16:38:07 +0100

    jokair_scripting.sh

--- jokair_scripting.sh
+++ jokair_scripting.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 clear 
-
-
 echo
 echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣄⣀"
 echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⠋⠙⢿⣷⣄"
@@ -49,193 +47,247 @@ echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁⠈⠙⢿⣿⣷⣶⣶⣶⣾⣿⡿�
 echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠈⢉⠉⠉⠉⢉⠀⢀⣀⡀"
 echo                                          
 echo
-
-
 echo "-----------------------------------------------------------"
 echo "                        TOOLS"
-echo "+-------+---------+------+--------+--------+--------------+"
-echo "/ nmap  / gobuster/ hydra / sqlmap / john   /  metasploit /"    
-echo "+-------+---------+------+--------+--------+--------------+"
-echo "/   1   /    2    /  3   /    4   /    5   /       6      /"
-echo "+-------+---------+------+--------+--------+--------------+"
-
-
-
+echo "+-------+---------+------+--------+--------+-------------------------+------------+"
+echo "/ nmap  / gobuster/ hydra / sqlmap / john   / john alternative script/metasploit /"    
+echo "+-------+---------+------+--------+--------+-------------------------+-----------+"
+echo "/   1   /    2    /  3   /    4   /    5   /             6          /      7    /"
+echo "+-------+---------+------+--------+--------+------------------------+-----------+"
 echo "choose a tool" 
 read tool_choice
-case $tool_choice in
-    1)
-            echo "nmap"
-        read ip_adress
-        nmap -A -Pn $ip_adress
-        ;;
-    2) 
-        echo "gobuster"
-        echo "2.1 directories enumeration"
-        echo "2.2 dns enumeration"
-        echo "2.3 vhost enumeration"
-
-        echo "choose an option"
-        read gobuster_option
-
-        case $gobuster_option in
-            2.1) 
-                echo "directory enumeration"
-                echo "choose dns or ip adress to enumerate"
-                read dns_or_ip_address
-                echo "put the path of you're wordlist"
-                read wordlist
-                gobuster dir -u http://$dns_or_ip_adress/ -w  $wordlist 
-                ;;
-            
-            2.2)
-                echo "domain name system enumeration"
-                echo "put the domain name"
-                read domain_name
-                echo "put the path of you're wordlist"
-                read wordlist
-                gobuster dns -d $domain_name -t 50 -w $wordlist
-                ;;
-            
-            2.3)
-                echo "vhost enumeration"
-                echo "put the domain name"
-                read domain_name
-                echo "put the path of you're wordlist"
-                read wordlist
-                gobuster vhost -u http://$domain_name/ -w $wordlist
-                ;;
-        esac
-        ;;
-    3)
-        echo "hydra"
-        echo "3.1 login ssh brute force authentication"
-        echo "3.2 login ftp brute force authentication"
-        echo "3.3 login mssql brute force authentication"
-        echo "3.4 login mysql brute force authentication"
-        echo "3.5 login smb brute force authentication"
-        echo "3.6 login rdp brute force authentication"
-        echo "3.7 login http basic authentication"
+if [ $tool_choice = 1] ; then
+    echo "nmap"
+    echo"ip adress"
+    read ip_adress
+    nmap -A -Pn $ip_adress
+elif [ $tool_choice = 2 ]; then   
+    echo "gobuster"
+    echo "2.1 directories enumeration"
+    echo "2.2 dns enumeration"
+    echo "2.3 vhost enumeration"
+    echo "choose an option"
+    read gobuster_option
+    if [ $gobuster_option = 2.1 ]; then
+            echo "directory enumeration"
+            echo "choose the url to enumerate"
+            read dns_or_ip_address
+            echo "put the path of you're wordlist"
+            read wordlist
+            gobuster dir -u $dns_or_ip_adress -w $wordlist 
+    elif [ $gobuster_option = 2.2 ]; then
+            echo "domain name system enumeration"
+            echo "put the domain name"
+            read domain_name
+            echo "put the path of you're wordlist"
+            read wordlist
+            gobuster dns -d $domain_name -t 50 -w $wordlist
+    elif [ $gobuster_option = 2.3 ]; then
+            echo "vhost enumeration"
+            echo "put the domain name"
+            read domain_name
+            echo "put the path of you're wordlist"
+            read wordlist
+            gobuster vhost -u http://$domain_name/ -w $wordlist
+    else 
+            echo "wrong input"
+    fi
+elif [ $tool_choice = 3 ]; then
+    echo "hydra"
+    echo "3.1 login ssh brute force authentication"
+    echo "3.2 login ftp brute force authentication"
+    echo "3.3 login mssql brute force authentication"
+    echo "3.4 login mysql brute force authentication"
+    echo "3.5 login smb brute force authentication"
+    echo "3.6 login rdp brute force authentication"
+    echo "3.7 login http basic authentication"
+    echo "3.8 login http login page authentication"
+    echo "choose an option"
+    read hydra_option
+    if [ $hydra_option = 3.1 ]; then
+        echo "ssh brute force"
+        echo "put the ip address"
+        read ip_address
+        echo "put the port"
+        read port
+        echo "put the username"
+        read username
+        echo "put the path of you're wordlist"
+        read wordlist
+        hydra -l $username -P $wordlist $ip_address:$port ssh -v
+    elif [ $hydra_option = 3.2 ]; then
+        echo "ftp brute force"
+        echo "put the ip address"
+        read ip_address
+        echo "put the port"
+        read port
+        echo "put the username"
+        read username
+        echo "put the path of you're wordlist"
+        read wordlist
+        hydra -l $username -P $wordlist ftp://$ip_address -v
+    elif [ $hydra_option = 3.3 ]; then
+        echo "mssql brute force"
+        echo "put the ip address"
+        read ip_address
+        echo "put the username"
+        read username
+        echo "put the path of you're wordlist"
+        read wordlist
+        hydra -l $username -P $wordlist mssql -v
+    elif [ $hydra_option = 3.4 ]; then
+        echo "mysql brute force"
+        echo "put the ip address"
+        read ip_address
+        echo "put the username"
+        read username
+        echo "put the path of you're wordlist"
+        read wordlist
+        hydra -l $username -P $wordlist mysql://$ip_address -v
+    elif [ $hydra_option = 3.5 ]; then                
+        echo "smb brute force"
+        echo "put the ip address"
+        read ip_address
+        echo "put the username"
+        read username
+        echo "put the path of you're wordlist"
+        read wordlist
+        hydra -l $username -P $wordlist smb://$ip_address -v
+    elif [ $hydra_option = 3.6 ]; then
+        echo "rdp brute force"
+        echo "put the ip address"
+        read ip_address
+        echo "put the username"
+        read username
+        echo "put the path of you're wordlist"
+        read wordlist
+        hydra -l $username -P $wordlist rdp://$ip_address -v
+    elif [ $hydra_option = 3.7 ]; then
+        echo "http basic brute force"
+        echo "put the ip address"
+        read ip_address
+        echo "put the port"
+        read port
+        echo "put the path of you're wordlist"
+        read wordlist
+        hydra -l $username -P $wordlist http://$ip_address -v http-get / 
+    elif [ $hydra_option = 3.8 ]; then 
         echo "3.8 login http login page authentication"
+        echo "put the ip address or dns"
+        read ip_or_dns
+        echo "put the username"
+        read username
+        echo "put the path of you're wordlist"
+        read wordlist
+        echo "put the form name"
+        read form_name
+        hydra -l $username -P $wordlist -f $ip_or_dns -s $port http-post-form "/$form_name:username=^USER^&password=^PASS^:F=<form name='login'"
+    else 
+        echo "invalid option"
+    fi
+elif [ $tool_choice = 4 ]; then
+    echo "sqlmap"
+    echo "4.1 scrappers located"
+    echo "4.2 crawler"
+    read sqlmap
+    if [ $sqlmap = 4.1 ]; then
+        echo "sqlmap scrappers located"
+        echo "put the url"
+        read url
+        sqlmap -u $url --batch --dump
+    elif [ $sqlmap = 4.2 ]; then  
+        echo "sqlmap crawlers"
+        echo "put the url"
+        read url
+        sqlmap -u $url --batch --crawl=2 --dump
+    else 
+        echo "wrong choice"
+    fi
 
-
-        echo "choose an option"
-        read hydra_option
-        
-        case $hydra_option in
-            3.1)
-                echo "ssh brute force"
-                echo "put the ip address"
-                read ip_address
-                echo "put the port"
-                read port
-                echo "put the username"
-                read username
-                echo "put the path of you're wordlist"
-                read wordlist
-                hydra -l $username -P $wordlist $ip_address:$port ssh -v
-                ;;
-
-            3.2)
-                echo "ftp brute force"
-                echo "put the ip address"
-                read ip_address
-                echo "put the port"
-                read port
-                echo "put the username"
-                read username
-                echo "put the path of you're wordlist"
-                read wordlist
-                hydra -l $username -P $wordlist ftp://$ip_address -v
-                ;;
-
-            3.3)
-                echo "mssql brute force"
-                echo "put the ip address"
-                read ip_address
-                echo "put the username"
-                read username
-                echo "put the path of you're wordlist"
-                read wordlist
-                hydra -l $username -P $wordlist mssql -v
-                ;;
-            3.4)
-                echo "mysql brute force"
-                echo "put the ip address"
-                read ip_address
-                echo "put the username"
-                read username
-                echo "put the path of you're wordlist"
-                read wordlist
-                hydra -l $username -P $wordlist mysql://$ip_address -v
-                ;;
-
-            3.5)
-                echo "smb brute force"
-                echo "put the ip address"
-                read ip_address
-                echo "put the username"
-                read username
-                echo "put the path of you're wordlist"
-                read wordlist
-                hydra -l $username -P $wordlist smb://$ip_address -v
-                ;;
-
-            3.6)
-                echo "rdp brute force"
-                echo "put the ip address"
-                read ip_address
-                echo "put the username"
-                read username
-                echo "put the path of you're wordlist"
-                read wordlist
-                hydra -l $username -P $wordlist rdp://$ip_address -v
-                ;;
-            3.7)
-                echo "http basic brute force"
-                echo "put the ip address"
-                read ip_address
-                echo "put the port"
-                read port
-                echo "put the path of you're wordlist"
-                read wordlist
-                hydra -l $username -P $wordlist http://$ip_address -v http-get / 
-                ;;
-            3.8)
-               echo "3.8 login http login page authentication"
-               echo "put the ip address or dns"
-               read ip_or_dns
-               echo "put the username"
-               read username
-               echo "put the path of you're wordlist"
-               read wordlist
-               echo "put the form name"
-               read form_name
-               hydra -l $username -P $wordlist -f $ip_or_dns -s $port http-post-form "/$form_name:username=^USER^&password=^PASS^:F=<form name='login'"
-                ;;
-        esac
-        ;;
-    4) 
-        echo "sqlmap"
-        echo "4.1 scrappers located"
-        echo "4.2 crawler"
-        read sqlmap
-        case "$sqlmap" in
-        4.1) 
-            echo "sqlmap scrappers located"
-            echo "put the url"
-            read url
-            sqlmap -u http://$url --batch --dump
-            ;;
-        4.2)  
-            echo "sqlmap crawlers"
-            echo "put the url"
-            read url
-            sqlmap -u http://$url --batch --crawl=2 --dump
-        esac
-        ;;
-    5)
-
-
-
+elif [ $tool_choice = 5 ]; then
+    echo "john"
+    echo "5.1 automatic crack hash"
+    echo "5.2 manual hashtype crack"
+    read john
+    if [ $john = 5.1 ]; then
+        echo "john automatic crack hash"
+        echo "put the file with the hash"
+        read hash_file
+        echo "wordlist file path"
+        read wordlist
+        john --wordlist= $wordlist  $hash_file
+    elif [ $john = 5.2 ]; then
+        echo "john manual crack hash"
+        echo "put the file with the hash"
+        read hash_file
+        echo "put the format of the hash"
+        read hash_format
+        echo "put the wordlist"
+        read wordlist
+        john --format= $hash_format  --wordlist= $wordlist  $hash_file
+    else
+        echo "choice incorect"
+    fi
+elif [ $tool_choice = 6 ]; then
+    echo "john script"
+    echo "6.1 ssh private key cracker"
+    echo "6.2 microsoft office protected file cracker"
+    echo "6.3 pdf protected file cracker"
+    echo "6.4 zip protected file cracker"
+    echo "6.5 bitlocker encrypted file cracker"
+    read john_script
+    if [ $john_script = 6.1 ]; then
+        echo "john ssh private key cracker"
+        echo "put the path of the encrypted private key"
+        read encrypted_private_key
+        echo "put the path of the wordlist"
+        read wordlist
+        ssh2john.py $encrypted_private_key > ssh.hash
+        john --wordlist= $wordlist ssh.hash
+        john ssh.hash --show
+    elif [ $john_script = 6.2 ]; then
+        echo "john microsoft office protected file cracker"
+        echo "put the encrypted file path"
+        read encrypted_file_path
+        echo "put the wordlist"
+        read wordlist
+        office2john.py $encrypted_file_path > protected-docx.hash
+        john --wordlist= $wordlist protected-docx.hash
+        john protected-docx.hash --show
+    elif [ $john_script = 6.3 ]; then
+        echo "pdf protected file cracker"
+        echo "put the encrypted file path"
+        read encrypted_file_path
+        echo "put the wordlist"
+        read wordlist
+        pdf2john.py $encrypted_file_path > pdf.hash
+        john --wordlist= $wordlist pdf.hash
+        john pdf.hash --show
+    elif [ $john_script = 6.4 ]; then
+        echo "zip protected file cracker"
+        echo "encrypted zip file"
+        read encrypted_file_zip
+        echo "put the wordlist"
+        read wordlist
+        zip2john.py $encrypted_file_zip > zip.hash
+        john --wordlist= $wordlist zip.hash
+        john zip.hash --show
+    elif [ $john_script = 6.5 ]; then
+        echo "bitlocker encrypted file cracker"
+        echo "put the encrypted file path"
+        read encrypted_file_path
+        echo "put the wordlist"
+        read wordlist
+        bitlocker2john -i Backup.vhd > backup.hashes
+        grep "bitlocker\$0" backup.hashes > backup.hash
+    else
+        echo "wrong choice"
+    fi
+elif [ $tool_choice = 7 ]; then
+    echo "metasploit"
+    msfconsole
+else
+    echo "wrong choice"
+fi
+$tool_choice
             
